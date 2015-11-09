e279f052f3d89171a9ddd1eda19a209dfa1a53bd
Rik Bähnemann <brik@student.ethz.ch>
2015-11-09 19:18:14 +0100

    goal biased rrt running

--- main.m
+++ main.m
@@ -1,4 +1,12 @@
 % source folder
 addpath(genpath(fileparts(which('main'))));
 
-% 
+% init
+clear all;
+close all;
+clc;
+
+% run naive RRT
+rrt = RRTPlanner();
+%rrt.setIncrement(1);
+rrt.solve;
--- planning/rrt/RRTPlanner.m
+++ planning/rrt/RRTPlanner.m
@@ -13,9 +13,12 @@ classdef RRTPlanner
         % default max. number vertices
         N = 2000;
         
-        % default incremental distance in meter
+        % default edge length in meter
         epsilon = 0.01;
         
+        % tree structure
+        rrt = struct('vertices',[],'numvertices',[], ...
+            'edges',[]);
     end
     
     methods
@@ -49,7 +52,7 @@ classdef RRTPlanner
 %% setMaxVertices
 % Set the maximum number of vertices allowed in tree
         function setMaxVertices(self,N)
-            if ( (N>0) & (mod(N,1)==0) )
+            if ( (N>0) && (mod(N,1)==0) )
                 self.N = N;
             else
                 error('N must be a positive integer.');
@@ -76,10 +79,118 @@ classdef RRTPlanner
                 return;
             end
         end        
+
+%% sample
+% Samples uniformly in state space. In expectation try to connect to the
+% goal every 100 samples.
+        function q_rand = sample(self)
+            x = self.space(1) + (self.space(2)-self.space(1))*rand;
+            y = self.space(3) + (self.space(4)-self.space(3))*rand;
+            q_rand = [x y];
+            if (rand>0.99)
+                q_rand = self.goal;
+            end
+        end
+
+%% addVertex
+% Adds a vertex to the tree.
+        function vertex_to_add = addVertex(self,q_new)
+            vertex_to_add = q_new;
+        end
+%% addEdge
+% Adds an edge from q_nearest to q_new to the tree.
+        function edge_to_add = addEdge(self,q_nearest_idx,q_new)
+            rrt_edge = q_new - self.rrt.vertices(q_nearest_idx);
+            edge_start_idx = q_nearest_idx;
+            edge_goal_idx = self.rrt.numvertices;
+            edge_to_add = table(rrt_edge, edge_start_idx, edge_goal_idx);
+        end
+%% findNearestVertex
+% Finds the nearest neighbor. Naive search.
+        function [q_nearest_idx, d_min] = findNearestVertex(self,q_rand)
+            d_min = Inf;
+            for i = 1:self.rrt.numvertices
+                d = norm(self.rrt.vertices(i,:) - q_rand);
+                if(d < d_min)
+                    d_min = d;
+                    q_nearest_idx = i;
+                end
+            end
+        end
+ %% connect
+ % connects the sampled vertex and the nearest vertex depending on their
+ % distance
+        function q_new = connect(self, q_nearest_idx, q_nearest_dist, q_rand)
+            if(q_nearest_dist>self.epsilon)
+                q_new = self.rrt.vertices(q_nearest_idx,:) + ...
+                    self.epsilon * ...
+                    (q_rand - self.rrt.vertices(q_nearest_idx,:)) / ...
+                    q_nearest_dist;
+            else
+                q_new = q_rand;
+            end
+        end
+     
+         
 %% solve
 % Initialize RRT, build tree and find solution
-        function result = solve(self)
+        function reached = solve(self)
+            t_rrt = tic;
             % init
+            % tree
+            self.rrt.vertices(1,:) = self.start;
+            self.rrt.numvertices = 1;
+            rrt_edge = [];
+            edge_start_idx = [];
+            edge_goal_idx = [];
+            self.rrt.edges = table(rrt_edge, edge_start_idx, edge_goal_idx);
+            % figure
+            figure();
+            hold on;
+            start_goal = [self.start; self.goal];
+            plot(start_goal(1,1),start_goal(1,2),'o','MarkerEdgeColor','k',...
+                'MarkerFaceColor','g','MarkerSize',10);
+            plot(start_goal(2,1),start_goal(2,2),'o','MarkerEdgeColor','k',...
+                'MarkerFaceColor','r','MarkerSize',10);
+            axis(self.space);
             
+            
+            reached = false;
+            i = 1;
+            % extend
+            while (~reached && i<=self.N)
+                % sample
+                q_rand = sample(self);
+                % nearest neighbor
+                [q_nearest_idx, q_nearest_dist] = findNearestVertex(self, q_rand);
+                % connect
+                q_new = connect(self, q_nearest_idx, q_nearest_dist, q_rand);
+                % addVertex
+                self.rrt.vertices(end+1,:) = addVertex(self,q_new);
+                self.rrt.numvertices = self.rrt.numvertices + 1;
+                % addEdge
+                self.rrt.edges = [self.rrt.edges; addEdge(self, q_nearest_idx, q_new)];
+                i = i + 1;
+                % check goal connection
+                if q_new == self.goal
+                    reached = true;
+                end
+                
+                % draw vertices and edges
+                edge_matrix = [self.rrt.vertices(self.rrt.edges.edge_start_idx(end),:);...
+                    self.rrt.vertices(self.rrt.edges.edge_goal_idx(end),:)];
+                plot(edge_matrix(:,1),edge_matrix(:,2),'color','k');
+                
+                
+            end            
+            t_elapsed = toc(t_rrt);
+            if reached
+                display('RRT solved!');
+                fprintf('Solution time: %.3f s \n', t_elapsed);
+            else
+                display('No solution found.'); 
+            end
+            fprintf('Number of vertices: %d \n', self.rrt.numvertices);
         end
     end
+end
